e986386764c6d904dbadfc286edfc9a2b6fa296d
FriendlyNGeeks <32318784+FriendlyNGeeks@users.noreply.github.com>
2021-04-23 13:26:10 -0400

    update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,7 +90,7 @@ var MyVue = new Vue({
         this.style_id = 'streets-v11';
         this.zoom = 15;
         this.attribution = 'Map data &copy; <a href="https://leafletjs.com/">Leaflet</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        this.mapbox_token = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+        this.mapbox_token = 'pk.eyJ1IjoiYW50enZpbGwiLCJhIjoiY2ttY21jaW1kMmN1aTJvcXM2bDI5MjZ0MiJ9.aEQeLPi8kHArXYUeUdqGCw';
         this.url = 'https://api.mapbox.com/styles/v1/mapbox/'+ this.style_id +'/static/{z}/{x}/{y}/600x600?access_token='+ this.mapbox_token+'';
         this.marker = L.latLng(35.083594,-106.6508292);
       },
